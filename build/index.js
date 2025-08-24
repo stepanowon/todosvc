df17742a09c867d83d420cdd75c19e6ee5ab77bd
stepanowon <stepanowon@hotmail.com>
2025-08-24 14:55:03 +0900

    body-parser 제거

--- build/index.js
+++ build/index.js
@@ -1,52 +1,39 @@
 "use strict";
 
 var _express = _interopRequireDefault(require("express"));
-var _bodyParser = _interopRequireDefault(require("body-parser"));
 var _cors = _interopRequireDefault(require("cors"));
 var _path = _interopRequireDefault(require("path"));
 var _routes = _interopRequireDefault(require("./routes"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-//import morgan from 'morgan';
-
-//import fs from 'fs';
-// import rfs from 'rotating-file-stream';
-
 var app = (0, _express["default"])();
 app.use((0, _cors["default"])());
-app.use(function (req, res, next) {
+
+// Cache control middleware
+app.use(function (_req, res, next) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
   next();
 });
-
-//-- 로깅
 var baseDir = _path["default"].resolve('.');
-
-// const logDirectory = path.join(baseDir, '/log')
-// fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-// var accessLogStream = rfs('access.log', {
-//   interval: '1d', // 매일 매일 로그 파일 생성
-//   path: logDirectory
-// })
-// app.use(morgan('combined', {stream: accessLogStream}))
-
 app.set('port', process.env.PORT || 3000);
 app.use(_express["default"]["static"](baseDir + '/public'));
 app.set('views', baseDir + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(_bodyParser["default"].json());
-app.use(_bodyParser["default"].urlencoded({
+app.use(_express["default"].json());
+app.use(_express["default"].urlencoded({
   extended: true
 }));
-app.use(function (req, res, next) {
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
-  next();
-});
 (0, _routes["default"])(app);
-var server = app.listen(app.get('port'), function () {
+
+// Error handling middleware
+app.use(function (err, _req, res, _next) {
+  console.error(err.stack);
+  res.status(500).json({
+    error: 'Something went wrong!'
+  });
+});
+app.listen(app.get('port'), function () {
   console.log("할일 목록 서비스가 " + app.get('port') + "번 포트에서 시작되었습니다!");
 });
--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
-//import morgan from 'morgan';
 import path from 'path';
-//import fs from 'fs';
-// import rfs from 'rotating-file-stream';
 import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 
-app.use(function (req, res, next) {
+// Cache control middleware
+app.use(function (_req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');
-    next()
+    next();
 });
 
-//-- 로깅
-var baseDir = path.resolve('.');
-
-// const logDirectory = path.join(baseDir, '/log')
-// fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-// var accessLogStream = rfs('access.log', {
-//   interval: '1d', // 매일 매일 로그 파일 생성
-//   path: logDirectory
-// })
-// app.use(morgan('combined', {stream: accessLogStream}))
+const baseDir = path.resolve('.');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -35,22 +23,19 @@ app.use(express.static(baseDir + '/public'));
 app.set('views', baseDir + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(function (req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next()
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 routes(app);
 
-const server = app.listen(app.get('port'), function() {
+// Error handling middleware
+app.use((err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.listen(app.get('port'), function() {
     console.log("할일 목록 서비스가 " + app.get('port') + "번 포트에서 시작되었습니다!");
 });
   
